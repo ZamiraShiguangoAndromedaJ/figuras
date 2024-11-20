c6b08e57696c1b97c8531ec8010f0d2dcfb33ffa
ZamiraShiguangoAndromedaJ <zamirashiguango@gmail.com>
2024-11-20 17:08:13 -0500

    creación del proyecto

---
+++ Documents/_fig/FigurasGeometricas/src/main/java/zamira/estudiante/figurasgeometricas/FigurasGeometricas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package zamira.estudiante.figurasgeometricas;
+
+/**
+ *
+ * @author user
+ */
+public class FigurasGeometricas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
