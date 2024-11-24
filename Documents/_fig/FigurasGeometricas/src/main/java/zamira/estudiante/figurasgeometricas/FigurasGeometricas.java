12c6d565f1c4173a73320d0b62f4d572f12433cb
ZamiraShiguangoAndromedaJ <zamirashiguango@gmail.com>
2024-11-24 01:35:31 -0500

    perimetro fallido

--- Documents/_fig/FigurasGeometricas/src/main/java/zamira/estudiante/figurasgeometricas/FigurasGeometricas.java
+++ Documents/_fig/FigurasGeometricas/src/main/java/zamira/estudiante/figurasgeometricas/FigurasGeometricas.java
@@ -32,16 +32,20 @@ public class FigurasGeometricas {
         
         System.out.println("Hola con la clase Triangulo Rectangulo jjeje");
         
-        TrianguloRectangulo objTrianguloRectangulo = new TrianguloRectangulo(7,8);
+        TrianguloRectangulo objTrianguloRectangulo = new TrianguloRectangulo(6,8);
         
         double area1=objTrianguloRectangulo.calcularAreaTrianguloRectangulo();
         System.out.println("El area del Triangulo Rectangulo es: " + area1);
         
         //double hipotenunsa=objTrianguloRectangulo.
         
+        double hipotenunsa=objTrianguloRectangulo.calcularHipotenunsaTrianguloRectangulo();
+        System.out.println("La Hipotenunsa de un Triangulo Rectangulo es: " + hipotenunsa);
         
         
-       
+      /*  double perimetro = objTrianguloRectangulo.calcularPerimetroTrianguloRectangulo();
+        System.out.println("El perimetro es: "+perimetro);
+       */
         
         
         
--- Documents/_fig/FigurasGeometricas/src/main/java/zamira/estudiante/figurasgeometricas/clases2/TrianguloRectangulo.java
+++ Documents/_fig/FigurasGeometricas/src/main/java/zamira/estudiante/figurasgeometricas/clases2/TrianguloRectangulo.java
@@ -9,31 +9,40 @@ package zamira.estudiante.figurasgeometricas.clases2;
  * @author user
  */
 public class TrianguloRectangulo {
-    
-    public double  baseCentimetro;
+
+    public double baseCentimetro;
     public double alturaCentimetro;
-    
-        public TrianguloRectangulo(double  baseCentimetro, double alturaCentimetro) {
-        this. baseCentimetro =  baseCentimetro;
+
+    public TrianguloRectangulo(double baseCentimetro, double alturaCentimetro) {
+        this.baseCentimetro = baseCentimetro;
         this.alturaCentimetro = alturaCentimetro;
 
     }
 
     public void imprimir() {
-        System.out.println(" baseCentimetro: " +  baseCentimetro);
-        System.out.println("alturaCentimetro: "+ alturaCentimetro);
+        System.out.println(" baseCentimetro: " + baseCentimetro);
+        System.out.println("alturaCentimetro: " + alturaCentimetro);
 
     }
 
     public double calcularAreaTrianguloRectangulo() {
-        
-        return (baseCentimetro*alturaCentimetro)/2;
+
+        return (baseCentimetro * alturaCentimetro) / 2;
 
     }
-    /*
+
+    public double calcularHipotenunsaTrianguloRectangulo() {
+
+        return Math.sqrt(Math.pow(baseCentimetro, 2) + Math.pow(alturaCentimetro, 2));
+        
+    }
+    
+    
     public double calcularPerimetroTrianguloRectangulo(){
         
-        return longitudLadoCentimetro*4;
-    }*/
+        double hipotenunsa = calcularHipotenunsaTrianguloRectangulo();
+        return baseCentimetro + alturaCentimetro + hipotenunsa;
+        
+    }
     
 }
